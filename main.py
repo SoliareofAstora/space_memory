d73aa1b954d61573d9e4fcbc1b7b401317c1e0ab
SoliareofAstora <piotr1kucharski@gmail.com>
2019-01-18 21:42:19 +0100

    KeyControll in C++, bolean data controll, missing library to build numpy

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import space_enviro.spaceLib as env
 
+
+# https://stackoverflow.com/questions/13207678/whats-the-simplest-way-of-detecting-keyboard-input-in-python-from-the-terminal/31736883
+# raw_input / input are asynchronus - good for game setup
+#
+
 b = env.hello("Hello World")
 b.greet()
+while b.stillPlaying():
+    b.display()
+
+print("Dome")
